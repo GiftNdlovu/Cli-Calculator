01f3f4bf2609e599a8f9e73de9d836205137a2aa
GiftNdlovu <hopthegamer0@gmail.com>
2025-01-07 22:14:11 +0200

    fixed the exiting of the script and stuff

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from colorama import Fore, Style
 print(Fore.RED + 'This is red text')
 print(Style.RESET_ALL + 'Back to normal text')
 
+#calculations
 def add(a, b):
     return a + b
-
 def subtract(a, b):
     return a - b
 
+#user options and such
 def main():
     print(Fore.CYAN + "choose type of operation")
     print(Style.RESET_ALL + "1. Addition")
@@ -22,19 +23,24 @@ def main():
     if choice == 1:
         results = add(a, b)
         print(Fore.GREEN + f"Results {results}")
+        print(Style.RESET_ALL + "..")
     elif choice == 2:
         results = subtract(a, b)
         print(Fore.GREEN + f"Results {results}")
+        print(Style.RESET_ALL + "..")
     else:
         print(Fore.RED + "invalid option")
+        print(Style.RESET_ALL + "..")
 
+#this is the exit mechanisim
 restart = True
 while restart:
-    main()
-
-check = input("Do you want to exit? (y / N)")
-if check == "N" or "n":
-    pass
-elif check == "Y" or "y":
-    exit()
+    check = input("Do you want to exit? (y / N)")
+    if check == "n":
+        main()
+    elif check == "Y" or "y":
+        exit()
+    else:
+        print("invalid option, continuing")
+        main()
 
